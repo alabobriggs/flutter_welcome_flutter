f4e8941bad80e41fb4f3b9082385069cfbfbf795
alabobriggs <alabobriggs@gmail.com>
2020-05-21 10:12:21 +0100

    add styling to text

--- lib/question.dart
+++ lib/question.dart
@@ -7,6 +7,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
