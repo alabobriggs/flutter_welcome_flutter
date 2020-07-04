fe73cd7a552ffc8d1827b100dec3a1aa0249c4c7
alabobriggs <alabobriggs@gmail.com>
2020-07-04 09:42:58 +0100

    add button action

--- lib/result.dart
+++ lib/result.dart
@@ -4,7 +4,9 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Yo have do it'),
-    );
+        child: Column(children: <Widget>[
+      Text('Yo have do it'),
+      FlatButton(onPressed: null, child: Text("Reset"))
+    ]));
   }
 }
